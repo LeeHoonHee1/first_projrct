36043122d04e4494c028d1813ce7e0c61086a215
seolsuyeon <123790554+seolsuyeon@users.noreply.github.com>
2024-09-05 13:55:11 +0900

    seol1

--- app/src/main/java/com/example/firstmyapplication/UI/DatabaseHelper.java
+++ app/src/main/java/com/example/firstmyapplication/UI/DatabaseHelper.java
@@ -1,9 +1,9 @@
 package com.example.firstmyapplication.UI;
 
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import java.io.File;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -11,11 +11,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // 내부 저장소의 지정된 경로에 데이터베이스 파일 생성
+        super(context, context.getDatabasePath(DATABASE_NAME).getPath(), null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        // 게시판 테이블 생성
         db.execSQL("CREATE TABLE posts (" +
                 "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "title TEXT, " +
@@ -24,7 +26,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // 테이블을 새로 생성하기 위해 기존 테이블 삭제
         db.execSQL("DROP TABLE IF EXISTS posts");
         onCreate(db);
     }
 }
+
